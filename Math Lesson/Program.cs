2efdb43f18871041728fced72f40e1537b01671a
YouAreALizardHarry <artemenko.sergey@yahoo.com>
2022-10-06 22:37:56 +0300

    new project

---
+++ Math Lesson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Math_Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
